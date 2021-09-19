7577221f9493c19b0ecf3e2babb13d8fa1f08b2b
QuinnG <quinngilbert12and3@gmail.com>
2021-09-18 23:53:06 -0400

    added comments

--- src/main/java/Solution07.java
+++ src/main/java/Solution07.java
@@ -1,11 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 7 Solution
+ *  Copyright 2021 Quinn Gilbert
+ */
 import java.util.*;
 public class Solution07 {
-
+    /*
+    prompt length
+    input length
+    prompt width
+    input width
+    create area in feet
+    area = length*width
+    create constant conversion rate
+    area in meters = area*conversion
+    print areas in both feet and meters
+    create constant conversion
+     */
 
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        final double feet_to_meters = 0.09290304;
         System.out.print("What is the length of the room in feet?");
         double length = in.nextDouble();
         System.out.print("What is the width of the room in feet?");
@@ -13,6 +27,7 @@ public class Solution07 {
         System.out.println("You entered dimensions of "+length+" feet by "+width+" feet.");
         double area = length*width;
         System.out.println("The area is "+area+" square feet,");
+        final double feet_to_meters = 0.09290304;
         double square_meters = area*feet_to_meters;
         System.out.println("or "+square_meters+" square meters");
 
